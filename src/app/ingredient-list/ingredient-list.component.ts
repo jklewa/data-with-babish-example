9356fafa9f1f3209fb35fe95bdb567da031548da
Jason Lewallen <jklewa@gmail.com>
2018-11-12 20:37:57 -0700

    ingredient list

--- src/app/ingredient-list/ingredient-list.component.ts
+++ src/app/ingredient-list/ingredient-list.component.ts
@@ -2,6 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EpisodeService, Episode } from '../episode.service';
 
+class Ingredient {
+  constructor(
+    public qty: string,
+    public unit: string,
+    public name: string,
+    public raw: string,
+    public episodeId: string,
+    public episodeName: string,
+    public recipeIdx: number,
+    public recipeName: string) {}
+}
+
 @Component({
   selector: 'app-ingredient-list',
   templateUrl: './ingredient-list.component.html',
@@ -10,6 +22,7 @@ import { EpisodeService, Episode } from '../episode.service';
 export class IngredientListComponent implements OnInit {
 
   ingredients: any[];
+  groupedIngredients: any;
 
   constructor(private episodeService: EpisodeService) { }
 
@@ -21,13 +34,33 @@ export class IngredientListComponent implements OnInit {
     this.episodeService.getEpisodes()
       .subscribe(
         (episodes: Episode[]) => {
-          const ingredients = [];
+          const ingredients: Ingredient[] = [];
 
-          episodes.map(ep => ep.recipes.map(recipe => {
-            ingredients.push(...recipe.ingredients);
-          }));
+          episodes.map(ep =>
+            ep.recipes.map((recipe, recipeIdx) =>
+              recipe.ingredients.map(i =>
+                ingredients.push(
+                  new Ingredient(i[0], i[1], i[2], i[3], ep.id, ep.episode_name, recipeIdx, recipe.method)
+                )
+              )
+            )
+          );
 
           this.ingredients = ingredients;
+
+          const groupedIngredients = {};
+
+          ingredients.reduce(
+            (acc, ing, idx, arr) => {
+              if (!acc[ing.name]) {
+                acc[ing.name] = [];
+              }
+              acc[ing.name].push(ing);
+              return acc;
+            },
+            groupedIngredients);
+
+          this.groupedIngredients = groupedIngredients;
         },
         (error) => { console.error('Failed to fetch episodes', error); });
   }
